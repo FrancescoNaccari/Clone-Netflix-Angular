d13c322c4d8c8341e3d73ff39bfb38a436e48477
FrancescoNaccari <francesco.n92@gmail.com>
2024-11-13 11:34:09 +0100

    aggiunta back end e sistemazione albero cartelle

---
+++ cloneNetflixBackEnd/src/main/java/it/cloneNetflixBackEnd/CloneNetflixBackEndApplication.java
@@ -0,0 +1,13 @@
+package it.cloneNetflixBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneNetflixBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneNetflixBackEndApplication.class, args);
+	}
+
+}
---
+++ cloneNetflixBackEnd/src/test/java/it/cloneNetflixBackEnd/CloneNetflixBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package it.cloneNetflixBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneNetflixBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
