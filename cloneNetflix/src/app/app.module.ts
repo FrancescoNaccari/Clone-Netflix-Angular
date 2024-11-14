a8e07517a3f841aab4b8c403865e134357252824
FrancescoNaccari <francesco.n92@gmail.com>
2024-11-14 10:47:15 +0100

    primi componet front

--- cloneNetflix/src/app/app.module.ts
+++ cloneNetflix/src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AccountComponent } from './components/account/account.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    AccountComponent,
+    ProfileComponent,
+    NavbarComponent,
+    
   ],
   imports: [
     BrowserModule,
