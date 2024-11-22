794cf6cc63745b77b872098b774817323c822a09
FrancescoNaccari <francesco.n92@gmail.com>
2024-11-22 11:16:07 +0100

    home V0.13 (ritorno alla V 0.11)

--- cloneNetflix/src/app/components/home/home.component.ts
+++ cloneNetflix/src/app/components/home/home.component.ts
@@ -1,22 +1,15 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnDestroy, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { forkJoin } from 'rxjs';
+import { Movie } from 'src/app/interface/movie.interface';
 import { MoviesService } from 'src/app/services/movies.service';
-
-interface MovieWithTrailer {
-  id: number;
-  media_type: string;
-  title: string;
-  name?: string;
-  overview: string;
-  poster_path: string;
-  backdrop_path: string;
-  genre_ids: number[];
+interface MovieWithTrailer extends Movie {
   showTrailer?: boolean;
   trailerUrl?: string | SafeResourceUrl;
   genres?: string;
   ageRating?: string;
   number_of_seasons?: number;
+
 }
 
 @Component({
@@ -27,20 +20,17 @@ interface MovieWithTrailer {
 export class HomeComponent implements OnInit, OnDestroy {
   popularMovies: MovieWithTrailer[] = [];
   featuredMovie: MovieWithTrailer | undefined;
-  movieCategories: { name: string; movies: MovieWithTrailer[] }[] = [];
-  carouselItems: any[] = [];
+  movieCategories: { name: string, movies: MovieWithTrailer[] }[] = [];
   private trailerTimeout: any;
-
   constructor(private moviesService: MoviesService, private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
     this.loadPopularMovies();
-    this.loadCarouselItems();
   }
 
   ngOnDestroy(): void {
-    clearTimeout(this.trailerTimeout);
-    this.popularMovies.forEach((movie) => {
+    // Clean up movie trailers when component is destroyed
+    this.popularMovies.forEach(movie => {
       movie.showTrailer = false;
       movie.trailerUrl = '';
     });
@@ -51,24 +41,19 @@ export class HomeComponent implements OnInit, OnDestroy {
       movies: this.moviesService.getPopularMovies(),
       tvShows: this.moviesService.getPopularTVShows(),
     }).subscribe(({ movies, tvShows }: any) => {
-      // Aggiungi il campo media_type
-      movies.results.forEach((item: any) => (item.media_type = 'movie'));
-      tvShows.results.forEach((item: any) => (item.media_type = 'tv'));
-
       const combinedResults = [...movies.results, ...tvShows.results];
       this.popularMovies = combinedResults.map((item: any) => {
         return {
           ...item,
-          title: item.title || item.name,
           genres: this.getGenresString(item.genre_ids ?? []),
           number_of_seasons: item.number_of_seasons,
         };
       });
 
-      // Imposta il featuredMovie
+      // Setting the featured movie for the cover
       this.featuredMovie = this.popularMovies[0];
 
-      // Crea le categorie
+      // Categorizing movies into sections
       this.movieCategories = [
         { name: 'Novità su Netflix', movies: this.popularMovies.slice(0, 10) },
         { name: 'Sport e fitness', movies: this.popularMovies.slice(10, 20) },
@@ -76,19 +61,7 @@ export class HomeComponent implements OnInit, OnDestroy {
       ];
     });
   }
-
-  loadCarouselItems(): void {
-    this.moviesService.getTrendingMovies().subscribe((response: any) => {
-      this.carouselItems = response.results.slice(0, 5).map((item: any) => ({
-        imageUrl: item.backdrop_path
-          ? `https://image.tmdb.org/t/p/original${item.backdrop_path}`
-          : 'URL_IMMAGINE_DI_DEFAULT', // Sostituisci con un'immagine di default se necessario
-        title: item.title || item.name,
-        description: item.overview,
-      }));
-    });
-  }
-
+  
   getGenresString(genreIds: number[]): string {
     const genreMap: { [key: number]: string } = {
       28: 'Azione',
@@ -111,21 +84,13 @@ export class HomeComponent implements OnInit, OnDestroy {
       10752: 'Guerra',
       37: 'Western',
     };
-
-    return genreIds.map((id) => genreMap[id]).filter(Boolean).join(' • ');
+    
+    return genreIds.map(id => genreMap[id]).filter(Boolean).join(' • ');
   }
 
   playTrailer(movie: MovieWithTrailer): void {
     this.trailerTimeout = setTimeout(() => {
-      let videoObservable;
-
-      if (movie.media_type === 'movie') {
-        videoObservable = this.moviesService.getMovieVideos(movie.id);
-      } else if (movie.media_type === 'tv') {
-        videoObservable = this.moviesService.getTVShowVideos(movie.id);
-      }
-
-      videoObservable?.subscribe((videos: any) => {
+      this.moviesService.getMovieVideos(movie.id).subscribe((videos: any) => {
         if (videos.results.length > 0) {
           const trailer = videos.results.find(
             (t: any) => t.type === 'Trailer' && t.site === 'YouTube'
@@ -140,10 +105,10 @@ export class HomeComponent implements OnInit, OnDestroy {
       });
     }, 300);
   }
-
   stopTrailer(movie: MovieWithTrailer): void {
     clearTimeout(this.trailerTimeout);
     movie.showTrailer = false;
     movie.trailerUrl = '';
   }
+  
 }
