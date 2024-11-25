c006ed4a6d964f233567ea4f695cb77663d12b1e
FrancescoNaccari <francesco.n92@gmail.com>
2024-11-25 19:09:29 +0100

    account V0.03

--- cloneNetflix/src/app/components/account/impostazioni/impostazioni.component.ts
+++ cloneNetflix/src/app/components/account/impostazioni/impostazioni.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./impostazioni.component.scss']
 })
 export class ImpostazioniComponent {
+  animazioni: boolean = false;
 
+  eliminaProfilo() {
+    const conferma = confirm('Sei sicuro di voler eliminare questo profilo?');
+    if (conferma) {
+      console.log('Profilo eliminato.');
+      // Aggiungi logica per eliminare il profilo
+    }
+  }
 }
